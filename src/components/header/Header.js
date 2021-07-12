08833d214735d1ee0e4bfd1afbbd6f0730420701
carloadamos <johncarloadamos@gmail.com>
2021-07-12 11:25:17 +0800

    Add title

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import ShoppingCart from "@material-ui/icons/ShoppingCart";
 import Home from "@material-ui/icons/Home";
@@ -12,9 +13,14 @@ const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
+  homeButton: {
+    marginRight: theme.spacing(2),
+    color: "white",
+  },
   cartButton: {
     marginRight: theme.spacing(2),
     color: "white",
+    flexGrow: 1,
   },
   title: {
     flexGrow: 1,
@@ -31,13 +37,16 @@ export const Header = () => {
           <Link to="/">
             <IconButton
               edge="start"
-              className={classes.cartButton}
+              className={classes.homeButton}
               color="inherit"
               aria-label="menu"
             >
               <Home />
             </IconButton>
           </Link>
+          <Typography variant="h6" className={classes.title}>
+            React Online Store
+          </Typography>
           <Link to="/cart">
             <IconButton
               edge="start"
